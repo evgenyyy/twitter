c68b79c5aa9e8dde8111504f3461ec56a9036a50
evgenyyy <e.likhoded@gmail.com>
2011-07-30 12:39:29 +0100

    Done with sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,12 +13,18 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      flash[:success] = "Welcome to Twitter!"
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       @title = "Sign up"
       render 'new'
     end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_path
   end
 
 end
