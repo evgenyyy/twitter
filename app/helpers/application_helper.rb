32be0b055895db2c39e4a242a92ff96bd5916cf8
evgenyyy <e.likhoded@gmail.com>
2011-07-31 01:04:50 +0100

    final app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-
+  
   # Return a title on a per-page basis.
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
@@ -9,9 +9,8 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
-
+  
   def logo
     image_tag("logo.png", :alt => "Sample App", :class => "round")
   end
-
 end
